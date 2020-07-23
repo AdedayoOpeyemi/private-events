f39f93f17ce155201457d92a624da6415dc571e0
Osama Aldemeery <aldemeery@gmail.com>
2020-07-23 18:33:49 +0200

    Add authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "users#profile"
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+
+  resources :users, except: [:index, :new]
 end
