46640cbc79c85cd051ebd77591afef37ed907109
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-07-27 20:53:41 +0100

    fixed linter errors

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,20 +1,21 @@
 module EventsHelper
+  def attend_unattend(event)
+    return attend_path(event) unless check
 
-    def attend_unattend(event)
-        return attend_path(event) unless check
-        if logged_user.attending?(event)
-            unattend_path(event)
-        else
-            attend_path(event)
-        end
+    if logged_user.attending?(event)
+      unattend_path(event)
+    else
+      attend_path(event)
     end
+  end
 
-    def switch_status(event)
-        return 'Attend' unless check
-        if logged_user.attending?(event)
-           return 'Unattend'
-        else
-            return 'Attend'
-        end
+  def switch_status(event)
+    return 'Attend' unless check
+
+    if logged_user.attending?(event)
+      'Unattend'
+    else
+      'Attend'
     end
+  end
 end
