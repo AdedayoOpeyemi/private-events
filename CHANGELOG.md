38ad8e4c2e6a3796b93519dbb43301cdece54e77
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-07-27 20:52:13 +0100

    Took out logic from view files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,3 +16,4 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Attend/Unattend events.
 - Separate attended from unattended events.
 - Separate upcoming from past events.
+- Used helpers for logic
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,20 @@
 module EventsHelper
+
+    def attend_unattend(event)
+        return attend_path(event) unless check
+        if logged_user.attending?(event)
+            unattend_path(event)
+        else
+            attend_path(event)
+        end
+    end
+
+    def switch_status(event)
+        return 'Attend' unless check
+        if logged_user.attending?(event)
+           return 'Unattend'
+        else
+            return 'Attend'
+        end
+    end
 end
